9d3135d59513536696589cd944eff6622fe37139
Gus Reid <gu5to@proton.me>
2023-10-02 03:26:07 -0600

    home changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "/index.html",
-    "/script.js"
+    "./index.html",
+    "./script.js"
   ],
   theme: {
     extend: {},
